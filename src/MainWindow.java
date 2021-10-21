2c205b30f00050625a8bb57baeaba1ba582c7b2f
aroman07 <87022612+aroman07@users.noreply.github.com>
2021-10-21 09:44:00 +0300

    Initial commit

---
+++ src/MainWindow.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+public class MainWindow {
+    private JButton button1;
+    private JPanel panel1;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("App");
+        frame.setContentPane(new MainWindow().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
